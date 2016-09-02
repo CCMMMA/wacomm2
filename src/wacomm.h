a1dc9ecb007990943ec61d693a8d8668f2440e55
Raffaele Montella <raffaele.montella@uniparthenope.it>
2016-09-02 21:33:32 +0200

    Updated

--- src/wacomm.h
+++ src/wacomm.h
@@ -1,7 +1,9 @@
 #ifndef WACOMM_H
 #define WACOMM_H
 
+
 #include <boost/multi_array.hpp>
+
 
 typedef boost::multi_array_ref<float, 1> float1d;
 typedef boost::multi_array_ref<float, 2> float2d;
@@ -9,4 +11,9 @@ typedef boost::multi_array_ref<float, 3> float3d;
 typedef boost::multi_array_ref<double, 1> double1d;
 typedef boost::multi_array_ref<double, 2> double2d;
 typedef boost::multi_array_ref<double, 3> double3d;
+
+template <typename T> int sgn(T val) {
+      return (T(0) < val) - (val < T(0));
+}
+
 #endif 
